0be57ff0379c67f75391de413d06e8dc90567270
step0035 <step0035@e.ntu.edu.sg>
2022-03-11 12:41:36 +0800

    SetRequestorCanConsent to true for lighting and ota-requestor apps

--- examples/lighting-app/ameba/main/chipinterface.cpp
+++ examples/lighting-app/ameba/main/chipinterface.cpp
@@ -97,6 +97,9 @@ extern "C" void amebaApplyUpdateCmdHandler()
 
 static void InitOTARequestor(void)
 {
+    // Set RequestorCanConsent to true
+    gRequestorCore.SetRequestorCanConsent(true);
+
     // Initialize and interconnect the Requestor and Image Processor objects -- START
     SetRequestorInstance(&gRequestorCore);
 
